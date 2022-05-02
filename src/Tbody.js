930c4594611ec0e7ee97ef098236328c14137ee6
riyad123456 <relaz051@uottawa.ca>
2022-05-01 22:09:38 -0400

    add appt for recept

--- src/Tbody.js
+++ src/Tbody.js
@@ -6,13 +6,15 @@ import Button from 'react-bootstrap/Button'
 
 
 var server = 'http://localhost:5001'
-
+var Ts = ["Appointment_ID","Patient_ID","Fee_charge_ID" , "Procedure_code", "Procedure_type", "Procedure_description",
+  "Tooth","Amount_of_procedures", "Total_charge", "Appointment_date"]
 
 
 function Tbody({data}) {
   const [items, setItems] = useState();
   var selectedRow = [];
   const [show, setShow] = useState(false);
+  const [show1, setShow1] = useState(false);
   const [currentItem, setCurrentItem] = useState({})
   const handleClose = () => {
     setShow(false);
@@ -20,6 +22,14 @@ function Tbody({data}) {
   }
   const  handleShow = (key)=>{
     setShow(true);
+    
+  }
+  const handleClose1 = () => {
+    setShow1(false);
+    
+  }
+  const  handleShow1 = ()=>{
+    setShow1(true);
     
   }
   const list = data.map(
@@ -31,7 +41,7 @@ function Tbody({data}) {
           
         )
        
-       return <Tr  index = {val} editClick={() => {handleShow(val);edit(data[val])}} delClick={() => deleteItem(data[val])} values= {li}/>
+       return <Tr  index = {val} editClick={() => {handleShow(val);edit(data[val])}} delClick={() => deleteItem(data[val])}  addapptClick = {() => {handleShow1();addAppt(data[val])}} values= {li}/>
     }
 )
 const getData = () => {
@@ -52,8 +62,37 @@ const getData = () => {
   
      
 }
+const getData1 = () => {
+  var elem = document.querySelector('#editForm1')
+  var count = 0;
+  var newAppt = {}
+  Object.keys(currentItem).map((key,value) => {
+    newAppt[key] = elem[count++].value
+})
+fetch(server+"/RDVAdd", { 
+  method: "POST" ,
+  headers: {
+        "Content-Type": "application/json",
+        "x-access-token": "token-value",
+      },
+      body: JSON.stringify(newAppt)
+}).then(setShow1(false))
+.then( window.location.reload(false))
+  
+  
+     
+}
 const editContent = Object.keys(currentItem).map((key,value) => {
   selectedRow.push(<FormGroup Key = {key} value={currentItem[key]}/>)
+})
+const addapptContent = Ts.map((key,value) => {
+  
+  if (key === "Patient_ID"){
+    return <><FormGroup  Key = {key} disabled = {true} value={currentItem["patient_id"]}/><br></br></>
+  }
+  else {
+  return <><FormGroup Key = {key} value={""}/><br></br></>
+  }
 })
 
   const deleteItem  = (dt) => {
@@ -70,6 +109,9 @@ const editContent = Object.keys(currentItem).map((key,value) => {
     
   }
   const edit = (dt) => {
+    setCurrentItem(dt)
+  }
+  const addAppt = (dt) => {
     setCurrentItem(dt)
   }
   const collectData = () => {
@@ -103,6 +145,25 @@ const editContent = Object.keys(currentItem).map((key,value) => {
           </Button>
         </Modal.Footer>
       </Modal>
+      <Modal show={show1} size="lg" onHide={handleClose1}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add appointment</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form id="editForm1"> 
+          {(addapptContent)}
+          </Form>
+          
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose1}>
+            Close
+          </Button>
+          <Button variant="primary"  onClick={getData1}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
 );
   
@@ -113,18 +174,19 @@ function Td({val}) {
       <td >{val}</td>
   );
 }
-function FormGroup({Key,value}){
+function FormGroup({Key,value,disabled}){
   return (
   <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
             <Form.Label >{Key}</Form.Label>
             <Form.Control class = {Key}
             defaultValue={value}
               autoFocus
+              disabled = {disabled}
             />
           </Form.Group>
           );
 }
-function Tr({values,delClick, editClick}){
+function Tr({values,delClick, editClick, addapptClick}){
  
   return (
   <tr>
@@ -141,6 +203,9 @@ function Tr({values,delClick, editClick}){
                           </li>
                           <li class="list-inline-item">
                           <Button variant="outline-danger" onClick = {delClick}>Delete</Button>
+                          </li>
+                          <li class="list-inline-item">
+                          <Button variant="outline-primary" onClick = {addapptClick}>Add appointment</Button>
                           </li>
                       </ul>
                   </td>
